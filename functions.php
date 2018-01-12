43a719886673cfa45ca47bbe4f944c9206364c2b
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2018-01-11 20:08:56 -0600

    Breadcrumb support
    Added breadcrumb support for site without Woo store (required Yoast SEO)

--- functions.php
+++ functions.php
@@ -6,3 +6,12 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
 // END ENQUEUE PARENT ACTION
+/**
+ * Prints the Breadcrumb in Storefront using the function by Yoast SEO.
+ */
+function storefront_yoast_breadcrumb() {
+	if ( function_exists( 'yoast_breadcrumb' ) && ! is_home() ) {
+		yoast_breadcrumb( '<nav class="breadcrumbs">','</nav>' );
+	}
+}
+add_action( 'storefront_content_top', 'storefront_yoast_breadcrumb' );
