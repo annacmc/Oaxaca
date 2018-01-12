14f0d359727df94a6d52afe97b805d4d2dc77950
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2018-01-11 20:41:56 -0600

    Popular Posts Grid Format
    Added styling for Popular Posts Widget Grid Format

--- functions.php
+++ functions.php
@@ -10,8 +10,23 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * Prints the Breadcrumb in Storefront using the function by Yoast SEO.
  */
 function storefront_yoast_breadcrumb() {
-	if ( function_exists( 'yoast_breadcrumb' ) && ! is_home() ) {
+	if ( function_exists( 'yoast_breadcrumb' )) {
 		yoast_breadcrumb( '<nav class="breadcrumbs">','</nav>' );
 	}
 }
 add_action( 'storefront_content_top', 'storefront_yoast_breadcrumb' );
+
+
+/**
+ * Adds a top bar to Storefront, before the header.
+ */
+function storefront_add_topbar() {
+    ?>
+    <div id="topbar">
+        <div class="col-full">
+            <p>Your text here</p>
+        </div>
+    </div>
+    <?php
+}
+add_action( 'storefront_before_header', 'storefront_add_topbar' );
