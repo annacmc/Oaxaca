ad8d1cc23d54eb1d325fe54d78e81ee9361e0999
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2018-01-13 18:22:27 -0600

    Remove Category Prefix
    Added function to remove 'Category:' prefix from category title display

--- functions.php
+++ functions.php
@@ -99,3 +99,13 @@ function create_collection_taxonomy() {
 		)
 	);
 }
+
+/* Remove Category Prefix */
+
+function prefix_category_title( $title ) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    }
+    return $title;
+}
+add_filter( 'get_the_archive_title', 'prefix_category_title' );
